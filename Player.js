4b1055e01fda60783b2a9f26ebd82ce87400a399
Komornikx <68018904+Komornikx@users.noreply.github.com>
2022-10-07 18:09:41 +0200

    updates

--- Player.js
+++ Player.js
@@ -1,20 +1,21 @@
 class Player {
-	constructor(id, x, y, name, color) {
-		this.id = id;
-		this.x = x;
-		this.y = y;
-		this.name = name;
-		this.color = color;
+  constructor(id, x, y, name, color) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.color = color;
 
-		this.tail = [];
-		this.points = 0;
-		this.size = 10;
+    this.tail = [];
+    this.points = 0;
+    this.size = 10;
 
-		this.mouseX = 0;
-		this.mouseY = 0;
-		this.diffX = 0;
-		this.diffY = 0;
-	}
+    this.speed = false;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.diffX = 0;
+    this.diffY = 0;
+  }
 }
 
 module.exports = Player;
--- gameServer.js
+++ gameServer.js
@@ -2,177 +2,192 @@ const Player = require('./Player');
 const Point = require('./Point');
 
 class GameServer {
-	constructor(io) {
-		this.io = io;
-		this.mapWidth = 1920;
-		this.mapHeight = 1080;
+  constructor(io) {
+    this.io = io;
+    this.mapWidth = 4000;
+    this.mapHeight = 4000;
 
-		this.running = false;
+    this.running = false;
 
-		this.players = [];
-		this.points = [];
+    this.players = [];
+    this.points = [];
 
-		this.io.on('connection', (socket) => {
-			socket.on('player-join', (data) => {
-				this.players.push(
-					new Player(socket.id, 40, 40, data.name, this.#getRandomColor())
-				);
+    this.io.on('connection', (socket) => {
+      socket.on('player-join', (data) => {
+        this.players.push(
+          new Player(
+            socket.id,
+            this.mapWidth / 2,
+            this.mapHeight / 2,
+            data.name,
+            this.#getRandomColor()
+          )
+        );
 
-				this.#emitUpdate();
-			});
+        this.#emitUpdate();
+      });
 
-			socket.on('change-dir', (data) => {
-				for (const player of this.players) {
-					if (player.id == socket.id) {
-						player.mouseX = data.mouseX;
-						player.mouseY = data.mouseY;
-					}
-				}
-			});
+      socket.on('player-speed', (data) => {
+        for (const player of this.players) {
+          if (player.id == socket.id) {
+            player.speed = data;
+          }
+        }
+      });
 
-			socket.on('disconnect', () => {
-				this.players = this.players.filter((el) => el.id != socket.id);
-			});
-		});
-	}
+      socket.on('change-dir', (data) => {
+        for (const player of this.players) {
+          if (player.id == socket.id) {
+            player.mouseX = data.mouseX;
+            player.mouseY = data.mouseY;
+          }
+        }
+      });
 
-	start() {
-		this.#mainLoop();
-		this.#generatePoints(100);
+      socket.on('disconnect', () => {
+        this.players = this.players.filter((el) => el.id != socket.id);
+        this.#emitUpdate();
+      });
+    });
+  }
 
-		this.running = true;
-	}
+  start() {
+    this.#mainLoop();
+    this.#generatePoints(300);
 
-	#mainLoop() {
-		setInterval(() => {
-			for (const player of this.players) {
-				//* move loop
-				player.diffX = player.mouseX - player.x;
-				player.diffY = player.mouseY - player.y;
+    this.running = true;
+  }
 
-				let pointDist = Math.sqrt(
-					player.diffX * player.diffX + player.diffY * player.diffY
-				);
+  #mainLoop() {
+    setInterval(() => {
+      for (const player of this.players) {
+        //* move loop
+        player.diffX = player.mouseX - player.x;
+        player.diffY = player.mouseY - player.y;
 
-				if (pointDist > 5) {
-					player.diffX *= 1 / pointDist;
-					player.diffY *= 1 / pointDist;
-				}
+        let pointDist = Math.sqrt(
+          player.diffX * player.diffX + player.diffY * player.diffY
+        );
 
-				player.x += player.diffX;
-				player.y += player.diffY;
+        if (pointDist > 5) {
+          player.diffX *= 1 / pointDist;
+          player.diffY *= 1 / pointDist;
+        }
 
-				if (player.diffX + player.diffY != 0) {
-					player.tail.push({
-						x: player.x,
-						y: player.y,
-						color: this.#getRandomColor(),
-					});
-				}
+        player.x += player.speed ? player.diffX * 2 : player.diffX;
+        player.y += player.speed ? player.diffY * 2 : player.diffY;
 
-				if (player.tail.length > player.points * 10) {
-					player.tail.shift();
-				}
+        if (player.diffX + player.diffY != 0) {
+          player.tail.push({
+            x: player.x,
+            y: player.y,
+            color: this.#getRandomColor(),
+          });
+        }
 
-				//* pick points detect
-				for (const point of this.points) {
-					if (
-						(player.x >= point.x || player.x + player.size >= point.x) &&
-						(player.y >= point.y || player.y + player.size >= point.y) &&
-						player.x <= point.x + point.size &&
-						player.y <= point.y + point.size
-					) {
-						this.#pickPoint(player, point);
-					}
-				}
+        if (player.tail.length > player.points * 10) {
+          player.tail.shift();
+        }
 
-				//* touch other player tail detect
-				for (const player2 of this.players) {
-					if (player.id != player2.id) {
-						for (const tailPart of player2.tail) {
-							if (
-								(player.x >= tailPart.x ||
-									player.x + player.size >= tailPart.x) &&
-								(player.y >= tailPart.y ||
-									player.y + player.size >= tailPart.y) &&
-								player.x <= tailPart.x + player2.size &&
-								player.y <= tailPart.y + player2.size
-							) {
-								this.#killPlayer(player);
-							}
-						}
-					}
-				}
+        //* pick points detect
+        for (const point of this.points) {
+          if (
+            (player.x >= point.x || player.x + player.size >= point.x) &&
+            (player.y >= point.y || player.y + player.size >= point.y) &&
+            player.x <= point.x + point.size &&
+            player.y <= point.y + point.size
+          ) {
+            this.#pickPoint(player, point);
+          }
+        }
 
-				player.size = this.#calculatePlayerSize(player);
-			}
-			this.#emitUpdate();
-		}, 10);
-	}
+        //* touch other player tail detect
+        for (const player2 of this.players) {
+          if (player.id != player2.id) {
+            for (const tailPart of player2.tail) {
+              if (
+                (player.x >= tailPart.x ||
+                  player.x + player.size >= tailPart.x) &&
+                (player.y >= tailPart.y ||
+                  player.y + player.size >= tailPart.y) &&
+                player.x <= tailPart.x + player2.size &&
+                player.y <= tailPart.y + player2.size
+              ) {
+                this.#killPlayer(player);
+              }
+            }
+          }
+        }
 
-	#emitUpdate() {
-		this.io.emit('update', {
-			players: this.players,
-			points: this.points,
-		});
-	}
+        player.size = this.#calculatePlayerSize(player);
+      }
+      this.#emitUpdate();
+    }, 10);
+  }
 
-	#killPlayer(player) {
-		const spawnPoints = player.points;
+  #emitUpdate() {
+    this.io.emit('update', {
+      players: this.players,
+      points: this.points,
+    });
+  }
 
-		this.players = this.players.filter((el) => el.id != player.id);
-	}
+  #killPlayer(player) {
+    const spawnPoints = player.points;
 
-	#pickPoint(player, point) {
-		this.points = this.points.filter((el) => el != point);
-		player.points++;
-		this.#generatePoint();
-		this.#emitUpdate();
-	}
+    this.players = this.players.filter((el) => el.id != player.id);
+  }
 
-	#generatePoints(amount) {
-		for (let x = 0; x <= amount; x++) {
-			this.#generatePoint();
-		}
-	}
+  #pickPoint(player, point) {
+    this.points = this.points.filter((el) => el != point);
+    player.points++;
+    this.#generatePoint();
+    this.#emitUpdate();
+  }
 
-	#generatePoint() {
-		const position = this.#getRandomPosition();
-		const size = this.#getRandomSize();
-		this.points.push(
-			new Point(position.x, position.y, size, this.#getRandomPointColor())
-		);
-	}
+  #generatePoints(amount) {
+    for (let x = 0; x <= amount; x++) {
+      this.#generatePoint();
+    }
+  }
 
-	#getRandomPosition() {
-		const maxX = this.mapWidth;
-		const maxY = this.mapHeight;
-		return {
-			x: Math.floor(Math.random() * (0 - maxX) + maxX),
-			y: Math.floor(Math.random() * (0 - maxY) + maxY),
-		};
-	}
+  #generatePoint() {
+    const position = this.#getRandomPosition();
+    const size = this.#getRandomSize();
+    this.points.push(
+      new Point(position.x, position.y, size, this.#getRandomPointColor())
+    );
+  }
 
-	#calculatePlayerSize(player) {
-		return (player.points < 10 ? 10 : player.points) * 1.1;
-	}
+  #getRandomPosition() {
+    const maxX = this.mapWidth;
+    const maxY = this.mapHeight;
+    return {
+      x: Math.floor(Math.random() * (0 - maxX) + maxX),
+      y: Math.floor(Math.random() * (0 - maxY) + maxY),
+    };
+  }
 
-	#getRandomSize() {
-		const min = 10;
-		const max = 20;
+  #calculatePlayerSize(player) {
+    return (player.points < 10 ? 10 : player.points) * 1.1;
+  }
 
-		return Math.floor(Math.random() * (min - max) + max);
-	}
+  #getRandomSize() {
+    const min = 10;
+    const max = 20;
 
-	// todo
-	#getRandomColor() {
-		const colors = ['#00e500', '#00cc00', '#00b200', '#009900', '#007f00'];
-		return colors[Math.floor(Math.random() * colors.length)];
-	}
+    return Math.floor(Math.random() * (min - max) + max);
+  }
 
-	#getRandomPointColor() {
-		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-	}
+  // todo
+  #getRandomColor() {
+    const colors = ['#00e500', '#00cc00', '#00b200', '#009900', '#007f00'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  #getRandomPointColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 }
 
 module.exports = GameServer;
